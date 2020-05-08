6161a67c71ecedca1b4ccfaae93fb9056e062132
y maru <naonydsa529@outlook.jp>
2020-05-08 18:01:08 +0900

    first commit

---
+++ TwoArrayToCSV.js
@@ -0,0 +1,15 @@
+function TwoArrayToCSV(data){
+  let csv = "";
+  for (var i =0 ; i < data.length; i++){
+    for (var j =0;j < data[i].length;j++){
+      if (j === data[i].length -1){
+        csv += data[i][j] + "\n";
+      }else{
+        csv += data[i][j] + ",";
+      }
+      
+    }
+  }
+  return csv;
+}
+
