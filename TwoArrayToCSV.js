e2a9429eaabef7c13cb67536d06173ba24ec02e7
maru008 <51712520+maru008@users.noreply.github.com>
2020-05-08 18:05:36 +0900

    Update TwoArrayToCSV.js

--- TwoArrayToCSV.js
+++ TwoArrayToCSV.js
@@ -2,12 +2,11 @@ function TwoArrayToCSV(data){
   let csv = "";
   for (var i =0 ; i < data.length; i++){
     for (var j =0;j < data[i].length;j++){
-      if (j === data[i].length -1){
+      if (j === data[i].length - 1){
         csv += data[i][j] + "\n";
       }else{
         csv += data[i][j] + ",";
       }
-      
     }
   }
   return csv;
